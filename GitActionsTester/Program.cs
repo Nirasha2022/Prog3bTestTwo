e8b07ea4f3c185a82063adcf3bf2000331c50b20
Nash <ramckurran@gmail.com>
2024-09-09 10:18:37 +0200

    FirtCommit

---
+++ GitActionsTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitActionsTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+            Console.WriteLine("BACKUP FIRST !!!");
+            Console.ReadLine();
+        }
+    }
+}
